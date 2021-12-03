c346ac60395c203b3caa8bcf4f746747d2ac7da5
Illarion <pro100illarion@gmail.com>
2021-12-03 15:42:36 +0300

    Added config project

--- Startup.cs
+++ Startup.cs
@@ -1,21 +1,29 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Company.Service;
 
 namespace Company
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration Configuration) =>
+            this.Configuration = Configuration;
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // подключение конфига из appsettings.json
+            Configuration.Bind("Project", new Config());
             // добавление поддержки MVC
             services.AddControllersWithViews()
                 // совместимость с asp.net core 3.0
